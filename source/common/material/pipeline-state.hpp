cf82ec26b3f47edc3a3b9af730b0b49eb4b72581
AliHithem <83958162+AliHithem@users.noreply.github.com>
2022-12-10 02:12:01 +0200

    Update pipeline-state.hpp
    last edit

--- source/common/material/pipeline-state.hpp
+++ source/common/material/pipeline-state.hpp
@@ -12,6 +12,11 @@ namespace our {
     struct PipelineState {
         // This set of pipeline options specifies whether face culling will be used or not and how it will be configured
         struct {
+            /*Try mentally visualizing a 3D cube and count the maximum number of faces you'll be able to see from any direction. If your imagination is not too creative you probably ended up with a maximum number of 3. You can view a cube from any position and/or direction, but you would never be able to see more than 3 faces. So why would we waste the effort of drawing those other 3 faces that we can't even see. If we could discard those in some way we would save more than 50% of this cube's total fragment shader runs
+            * save more than 50%.
+            * face culling does. OpenGL checks all the faces that are front facing towards the viewer and renders those while discarding all the faces that are back facing
+            */
+            
             bool enabled = false;
             GLenum culledFace = GL_BACK;
             GLenum frontFace = GL_CCW;
@@ -19,12 +24,23 @@ namespace our {
 
         // This set of pipeline options specifies whether depth testing will be used or not and how it will be configured
         struct {
+            /*In most systems you'll see a depth buffer with a precision of 24 bits.
+            *Depth testing is done in screen space after the fragment shader has run
+            *GL_EQUAL->Passes if the fragment's depth value is equal to the stored depth value.
+            */
             bool enabled = false;
             GLenum function = GL_LEQUAL;
         } depthTesting;
 
         // This set of pipeline options specifies whether blending will be used or not and how it will be configured
         struct {
+            /*Blending in OpenGL is commonly known as the technique to implement transparency within objects
+            * The amount of transparency of an object is defined by its color's alpha value
+            * alpha value of 0.0 would result in the object having complete transparency
+            * Cresult=Csource∗Fsource+Cdestination∗Fdestination
+            * Csource= the source color vector. This is the color output of the fragment shader.
+            * Fsource=the source factor value. Sets the impact of the alpha value on the source color.
+            */
             bool enabled = false;
             GLenum equation = GL_FUNC_ADD;
             GLenum sourceFactor = GL_SRC_ALPHA;
@@ -48,8 +64,9 @@ namespace our {
                 glFrontFace(faceCulling.frontFace); // Specify which face to be rendered (counter clock wise or clockwise)
             }
             else
+            {
                 glDisable(GL_CULL_FACE);
-
+            }
             if (depthTesting.enabled) {
                 glEnable(GL_DEPTH_TEST);    // Enable depth testing to be used
                 glDepthFunc(depthTesting.function); // Specify when to replace the written depth value with the new value
@@ -60,8 +77,7 @@ namespace our {
             }
 
             if (blending.enabled) {
-                glEnable(GL_BLEND);
-                // Specify the the source and destination blending factors according to "constant color" and it is defaulted to be 0.
+                glEnable(GL_BLEND);// Specify the the source and destination blending factors according to "constant color" and it is defaulted to be 0.
                 glBlendColor(blending.constantColor[0], blending.constantColor[1], blending.constantColor[2], blending.constantColor[3]);
                 glBlendEquation(blending.equation); // Add the two colors together including the factor (GL_FUNC_ADD)
                 glBlendFunc(blending.sourceFactor, blending.destinationFactor); // Specify the factor for source color and destination color
