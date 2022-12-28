98c02c776645366e8bfebf942529f754c05b000d
Ahmed Ayman Saad <34175435+AhmedAymanSaad@users.noreply.github.com>
2022-12-28 21:01:14 +0200

    removed coin fixed bug

--- source/states/play-state.hpp
+++ source/states/play-state.hpp
@@ -32,7 +32,6 @@ class Playstate: public our::State {
         }
         // We initialize the camera controller system since it needs a pointer to the app
         cameraController.enter(getApp());
-        freeCameraController.enter(getApp());
         // Then we initialize the renderer
         auto size = getApp()->getFrameBufferSize();
         renderer.initialize(size, config["renderer"]);
@@ -42,7 +41,6 @@ class Playstate: public our::State {
         // Here, we just run a bunch of systems to control the world logic
         movementSystem.update(&world, (float)deltaTime);
         cameraController.update(&world, (float)deltaTime);
-        freeCameraController.update(&world, (float)deltaTime);
         // And finally we use the renderer system to draw the scene
         renderer.render(&world);
 
@@ -60,7 +58,6 @@ class Playstate: public our::State {
         renderer.destroy();
         // On exit, we call exit for the camera controller system to make sure that the mouse is unlocked
         cameraController.exit();
-        freeCameraController.exit();
         // Clear the world
         world.clear();
         // and we delete all the loaded assets to free memory on the RAM and the VRAM
