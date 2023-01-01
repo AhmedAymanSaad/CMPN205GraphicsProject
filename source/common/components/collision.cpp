6821a5cfb702422bfdb672be0dc0b42684152531
ArwaShamardal <76781020+ArwaShamardal@users.noreply.github.com>
2023-01-01 16:22:49 +0200

    added documentation for components

--- source/common/components/collision.cpp
+++ source/common/components/collision.cpp
@@ -5,6 +5,9 @@
 namespace our {
 
     // Reads the bounding box from the given json object
+    // where data.value("x", x) means:
+    // if json data file has a key called "x", then use its value, otherwise use the default value
+    // saved as the second parameter
     void CollisionComponent::deserialize(const nlohmann::json& data) {
         if (!data.is_object()) return;
         boundingBox = data.value ("boundingBox", glm::vec3(0.0f, 0.0f, 0.0f));
--- source/common/components/player-controller.hpp
+++ source/common/components/player-controller.hpp
@@ -6,11 +6,9 @@
 
 namespace our {
 
-    // This component denotes that the FreeCameraControllerSystem will move the owning entity using user inputs.
-    // It will also control the camera field of view accroding to the scrolling of the mouse wheel
-    // This component is added as a slightly complex example for how use the ECS framework to implement logic.
-    // For more information, see "common/systems/free-camera-controller.hpp"
-    // For a more simple example of how to use the ECS framework, see "movement.hpp"
+    // This component denotes that the PlayerControllerComponent will move the owning entity using user inputs.
+    // the child of the player would the camera to make a 3rd person view
+    // we did that so that we can have the option to change camera view without changing the player position
     class PlayerControllerComponent : public Component {
     public:
         // The senstivity paramter defined sensitive the camera rotation & fov is to the mouse moves and wheel scrolling
@@ -19,7 +17,7 @@ namespace our {
         glm::vec3 positionSensitivity = {3.0f, 3.0f, 3.0f}; // The unity per second of camera movement if WASD is pressed
         float speedupFactor = 5.0f; // A multiplier for the positionSensitivity if "Left Shift" is held.
 
-        int coinsCollected = 0; // The number of coins collected
+        int coinsCollected = 0; // The number of coins collected - for game logic
 
         // The ID of this component type is "Free Camera Controller"
         static std::string getID() { return "Player Controller"; }
